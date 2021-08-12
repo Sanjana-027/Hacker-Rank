101d41ce2d84bcc44317add95fa6449a5b3af188
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2021-08-12 11:58:33 +0530

    Create 01. Java Stdin and Stdout I.java

---
+++ Java Basics/1.Introduction/01. Java Stdin and Stdout I.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        
+        System.out.print(num1 + "\n" + num2 + "\n" + num3);
+    }
+}
